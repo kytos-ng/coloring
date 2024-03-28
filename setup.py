e5bd7339fd3e852c245c3e8b9259e627e19f57b8
Aldo Ortega <aortegay@fiu.edu>
2024-03-28 10:14:08 -0400

    Updated readme

--- setup.py
+++ setup.py
@@ -274,7 +274,7 @@ setup(name=f'{NAPP_USERNAME}_{NAPP_NAME}',
       author='Amlight Team',
       author_email='antonio@amlight.net',
       license='MIT',
-      install_requires=read_requirements() + ["setuptools >= 59.6.0"],
+      install_requires=read_requirements() + ['importlib_metadata'],
       packages=[],
       cmdclass={
           'clean': Cleaner,
